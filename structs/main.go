5b90cb59fce8bc7b47cd456172a5606f5aa2cf59
KadirZukorlic <kadirzukorlic@gmail.com>
2024-10-02 00:01:47 +0200

    Pointers

--- structs/main.go
+++ structs/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+	nahla := person{
+		firstName: "Nahla",
+		lastName:  "Zukorlić",
+		contactInfo: contactInfo{
+			email:   "nahlazukorlic@gmail.com",
+			zipCode: 36320,
+		},
+	}
+	nahlaPointer := &nahla
+	nahlaPointer.changeName("Hamza")
+	nahla.print()
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+func (pointerToPerson *person) changeName(newName string) {
+	(*pointerToPerson).firstName = newName
 }
