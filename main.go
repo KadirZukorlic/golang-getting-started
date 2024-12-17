0a8baf449aaf15284f3adc936f18bc1a949c81da
KadirZukorlic <kadirzukorlic@gmail.com>
2024-12-17 03:35:40 +0100

    Add wait group to save full response once goroutines are finished

--- main.go
+++ main.go
@@ -6,37 +6,54 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"sync"
 	"time"
 )
 
 func main() {
+	file, err := os.Create("output.txt")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+
+	fmt.Fprintln(file, "Starting the program...")
+
 	start := time.Now()
 	ch := make(chan string)
 
+	var wg sync.WaitGroup
+
 	for _, url := range os.Args[1:] {
-		go fetch(url, ch) // start a goroutine
+		wg.Add(1)
+		go fetch(url, ch, &wg)
 	}
 
-	for range os.Args[1:] {
-		fmt.Println(<-ch) // receive from channel ch
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	for msg := range ch {
+		fmt.Fprintln(file, msg)
 	}
 
-	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
-
+	fmt.Fprintf(file, "%.2fs elapsed\n", time.Since(start).Seconds())
 }
 
-func fetch(url string, ch chan<- string) {
+func fetch(url string, ch chan<- string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	start := time.Now()
 	resp, err := http.Get(url)
-
 	if err != nil {
-		ch <- fmt.Sprint(err) // send to channel ch
+		ch <- fmt.Sprint(err)
 		return
 	}
+	defer resp.Body.Close()
 
 	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
-	resp.Body.Close() // don't leak resources
-
 	if err != nil {
 		ch <- fmt.Sprintf("while reading %s: %v", url, err)
 		return
@@ -44,5 +61,4 @@ func fetch(url string, ch chan<- string) {
 
 	secs := time.Since(start).Seconds()
 	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
-
 }
